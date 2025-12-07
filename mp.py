e5468c2ba73226ce6b7aa7f0dbf20da72d473d14
ya27hw <yousuf5550@gmail.com>
2025-12-07 04:22:42 +0400

    cleanup code

--- mp.py
+++ mp.py
@@ -7,10 +7,6 @@ import mediapipe as mp
 
 mp_face_mesh = mp.solutions.face_mesh
 
-# def euclidean(a, b):
-#     return np.linalg.norm(a - b)
-
-
 # Used to keep track of how many frames the eye/mouth has exceeded limits
 eye_counter = 0
 mouth_counter = 0
@@ -19,16 +15,12 @@ mouth_alarm = False
 
 # Starting the live video stream
 video_stream = cv2.VideoCapture(constants.CAM_INDEX)
-# Set the width (CAP_PROP_FRAME_WIDTH = 3)
 video_stream.set(cv2.CAP_PROP_FRAME_WIDTH, constants.WIDTH)
-
-# Set the height (CAP_PROP_FRAME_HEIGHT = 4)
 video_stream.set(cv2.CAP_PROP_FRAME_HEIGHT, constants.HEIGHT)
 
 if not video_stream.isOpened():
     raise Exception(
         "Could not open video stream. Please check your camera index")
-    exit(1)
 
 print("Starting camera... Press 'ESC' to quit.")
 
@@ -38,12 +30,17 @@ with mp_face_mesh.FaceMesh(
     refine_landmarks=True,
 ) as face_mesh:
     while True:
+        fps = video_stream.get(cv2.CAP_PROP_FPS)
+        frame_num = video_stream.get(cv2.CAP_PROP_POS_FRAMES)
+        time_sec = frame_num / fps
+
         # Read the next frame from the stream
         ret, frame = video_stream.read()
         if not ret:
             print("Error reading frame.")
             break
 
+        # Display the frame in RGB.
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = face_mesh.process(frame)
 
@@ -52,6 +49,7 @@ with mp_face_mesh.FaceMesh(
 
         lm = results.multi_face_landmarks[0].landmark
 
+        # Get eye and mouth aspect ratios
         left_eye_points = extract_points(lm, constants.LEFT_EYE_IDX)
         right_eye_points = extract_points(lm, constants.RIGHT_EYE_IDX)
         left_ear = eye_aspect_ratio(left_eye_points)
@@ -60,6 +58,7 @@ with mp_face_mesh.FaceMesh(
         ear = (left_ear + right_ear) / 2
         mar = mouth_aspect_ratio(lm)
 
+        # Check if eye/mouth are drowsy if they exceed their respective limits
         if ear < constants.EYE_RATIO_LIMIT:
             eye_counter += 1
             if eye_counter >= constants.EYE_RATIO_CONSECUTIVE_FRAMES:
@@ -84,7 +83,7 @@ with mp_face_mesh.FaceMesh(
             mouth_counter = 0
             mouth_alarm = False
 
-        # Draw eyes
+        # Draw the outline of the eyes and mouth onto the frame.
         draw_hull(cv2, frame, lm, constants.LEFT_EYE_IDX)
         draw_hull(cv2, frame, lm, constants.RIGHT_EYE_IDX)
         draw_hull(cv2, frame, lm, constants.MOUTH_IDX)
@@ -93,6 +92,10 @@ with mp_face_mesh.FaceMesh(
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
         cv2.putText(frame, f"MAR: {mar:.2f}", (300, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+
+        #
+        cv2.putText(frame, f"FPS: {fps}", (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
         cv2.imshow("DDAS - Face Tracking",
